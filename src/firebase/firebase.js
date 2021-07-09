9573ad6e05631920935566f6ac4ac2f2c21b10ea
Miguel-esquivel <65308563+Miguel-esquivel@users.noreply.github.com>
2021-07-09 12:02:13 -0400

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBsmXay7Ls0rIoA6j7WhdWtMJrPnPY9C9w",
-    authDomain: "turnero-d1e04.firebaseapp.com",
-    projectId: "turnero-d1e04",
-    storageBucket: "turnero-d1e04.appspot.com",
-    messagingSenderId: "1021966319948",
-    appId: "1:1021966319948:web:de0d148775b59c6e298fe9",
-    measurementId: "G-PR728CRS3V"
+    tu configuracion
 };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
